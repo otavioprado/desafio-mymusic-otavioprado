bf81eeddddf2107cf56ba0fbb654e2528b53a56c
Otávio Prado <otaviofelipedoprado@gmail.com>
2018-09-21 21:27:49 -0300

    Ajustando API de músicas para retornar todas as músicas ou filtrar considerando o valor informado

--- src/main/java/com/handson/repository/MusicasRepository.java
+++ src/main/java/com/handson/repository/MusicasRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.handson.model.Musicas;
@@ -11,7 +12,7 @@ import com.handson.model.Musicas;
 @Repository
 public interface MusicasRepository extends JpaRepository<Musicas, String> {
 
-	@Query(value = "SELECT m FROM Musicas m JOIN m.artista") 
-    public List<Musicas> findAll2();
+	@Query(value = "SELECT m FROM Musicas m JOIN m.artista a WHERE a.nome = :nomeArtista") 
+    public List<Musicas> findAllByArtistName(@Param("nomeArtista") String filterName);
 	
 }
--- src/main/java/com/handson/service/MusicaServiceBean.java
+++ src/main/java/com/handson/service/MusicaServiceBean.java
@@ -2,6 +2,8 @@ package com.handson.service;
 
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,11 +13,21 @@ import com.handson.repository.MusicasRepository;
 @Service
 public class MusicaServiceBean implements MusicaService {
 	
+	private static final Logger LOG = Logger.getLogger(MusicaServiceBean.class);
+
 	@Autowired
 	private MusicasRepository musicaRepository;
-    
-    public List<Musicas> listarMusicas(String filtro){
-    	return musicaRepository.findAll2();
-    }
+
+	public List<Musicas> listarMusicas(String filtro) {
+		
+		LOG.info("Iniciando busca de músicas");
+		
+		if(StringUtils.isBlank(filtro)) {
+			LOG.info("Nenhum filtro foi informando, buscando todas as músicas...");
+			return musicaRepository.findAll();
+		}
+		
+		return musicaRepository.findAllByArtistName(filtro);
+	}
 
 }
